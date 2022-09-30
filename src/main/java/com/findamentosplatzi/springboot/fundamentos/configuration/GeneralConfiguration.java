0b804004a3cda0a0c5ae350e3893c9b31dff0808
Jaime Norato <jn901113jn@gmail.com>
2022-09-30 16:33:05 -0500

    * configuracion de datasource a nivel de properties y clases

--- src/main/java/com/findamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/findamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -3,8 +3,12 @@ package com.findamentosplatzi.springboot.fundamentos.configuration;
 import com.findamentosplatzi.springboot.fundamentos.bean.MyBeanWhitProperties;
 import com.findamentosplatzi.springboot.fundamentos.bean.MyBeanWhitPropertiesImplement;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class GeneralConfiguration {
@@ -20,5 +24,15 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWhitProperties function() {
         return new MyBeanWhitPropertiesImplement(name, apellido);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 }
