77fce3071b171c95dd491d2e5e7d335fffbfa5c9
Jaime Norato <jn901113jn@gmail.com>
2022-10-01 16:22:28 -0500

    * usando anotacion value para cargar variables de entorno desde un archivo de properties propio

--- src/main/java/com/findamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
+++ src/main/java/com/findamentosplatzi/springboot/fundamentos/configuration/GeneralConfiguration.java
@@ -2,15 +2,20 @@ package com.findamentosplatzi.springboot.fundamentos.configuration;
 
 import com.findamentosplatzi.springboot.fundamentos.bean.MyBeanWhitProperties;
 import com.findamentosplatzi.springboot.fundamentos.bean.MyBeanWhitPropertiesImplement;
+import com.findamentosplatzi.springboot.fundamentos.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 import javax.xml.crypto.Data;
 
 @Configuration
+@PropertySource("classpath:connection.properties")
+@EnableConfigurationProperties(UserPojo.class)
 public class GeneralConfiguration {
     @Value("${value.name}")
     private String name;
@@ -21,6 +26,15 @@ public class GeneralConfiguration {
     @Value("${value.random}")
     private String random;
 
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${username}")
+    private String username;
+    @Value("${password}")
+    private String password;
+    @Value("${driver}")
+    private String driver;
+
     @Bean
     public MyBeanWhitProperties function() {
         return new MyBeanWhitPropertiesImplement(name, apellido);
@@ -29,10 +43,10 @@ public class GeneralConfiguration {
     @Bean
     public DataSource dataSource() {
         DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
-        dataSourceBuilder.driverClassName("org.h2.Driver");
-        dataSourceBuilder.url("jdbc:h2:mem:testdb");
-        dataSourceBuilder.username("sa");
-        dataSourceBuilder.password("");
+        dataSourceBuilder.driverClassName(driver);
+        dataSourceBuilder.url(jdbcUrl);
+        dataSourceBuilder.username(username);
+        dataSourceBuilder.password(password);
         return dataSourceBuilder.build();
     }
 }
